0a68f0982401dd366b23288d3a3a7e06032563a6
Franco <francoafalconieri@gmail.com>
2026-05-05 18:05:45 -0300

    Arreglado el sobrefiltrado, le robé el filtro a nachito

--- 01/Main.m
+++ 01/Main.m
@@ -4,15 +4,8 @@ M01 = cargamediciones("../m01/");
 M02 = cargamediciones("../m02/");
 M03 = cargamediciones("../m03/");
 M04 = cargamediciones("../m04/");
-% 2_ Filtrado
-% definicion de un filtro butterworth
-filtro.fs = 1/mean(diff(M01.ts));
-filtro.fc = 10;
-filtro.orden = 20;
-[filtro.b, filtro.a] = butter(filtro.orden, ...
-    filtro.fc/(filtro.fs/2),"low");
-% 3_ Aproximacion de valores
-M01 = derivadormaestro(M01,filtro);
-M02 = derivadormaestro(M02,filtro);
-M03 = derivadormaestro(M03,filtro);
-M04 = derivadormaestro(M04,filtro);
+% 2_ Aproximacion de valores
+M01 = derivadormaestro(M01);
+M02 = derivadormaestro(M02);
+M03 = derivadormaestro(M03);
+M04 = derivadormaestro(M04);
--- 01/derivadormaestro.m
+++ 01/derivadormaestro.m
@@ -1,18 +1,22 @@
-function Mxx = derivadormaestro(Mxx,f)
+function Mxx = derivadormaestro(Mxx)
 % obtiene velocidad y aceleracion de la barra y el
 % disco de inercia, mediante el promedio de las 
 % diferencias finita ascendente y descendiente
-Mxx.tita1 = filtfilt(f.b,f.a,Mxx.tita1);
-Mxx.tita2 = filtfilt(f.b,f.a,Mxx.tita2);
+
+% definicion de un filtro butterworth
+filtro.fs = 1/mean(diff(Mxx.ts));
+filtro.fc = .1;
+filtro.orden = 2;
+[b, a] = butter(filtro.orden,filtro.fc);
 
 Mxx.v1 = finitopromedio(Mxx.tita1,Mxx.ts);
-Mxx.v1 = filtfilt(f.b,    f.a,    Mxx.v1);
+Mxx.v1 = filtfilt(b,        a,    Mxx.v1);
 Mxx.v2 = finitopromedio(Mxx.tita1,Mxx.ts);
-Mxx.v2 = filtfilt(f.b,    f.a,    Mxx.v2);
+Mxx.v2 = filtfilt(b,        a,    Mxx.v2);
 
 Mxx.a1 = finitopromedio(Mxx.v1,   Mxx.ts);
-Mxx.a1 = filtfilt(f.b,    f.a,    Mxx.a1);
+Mxx.a1 = filtfilt(b,       a,    Mxx.a1);
 Mxx.a2 = finitopromedio(Mxx.v1,   Mxx.ts);
-Mxx.a2 = filtfilt(f.b,    f.a,    Mxx.a2);
+Mxx.a2 = filtfilt(b,    a,    Mxx.a2);
 
 end
